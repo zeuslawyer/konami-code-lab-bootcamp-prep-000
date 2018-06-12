ecee1160791cb158594b6d9d3c4c17ecaccac9ad
zubin.pratap@gmail.com <zubin.pratap@gmail.com>
2018-06-12 02:15:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,16 +14,27 @@ const codes = [
 function init() {
   // your code here
   let body= document.querySelector('body');
+  let keyPressNum = 0;
+  let correctKeys = 0;
   body.addEventListener('keydown', function(e){
-    let keyPressNum = 0;
-    let correctKeys = 0;
+    console.log(e.key, keyPressNum );
     if (e.key === codes[keyPressNum]) {
-      correctKeys+=1;
+      correctKeys+=1; 
+      console.log(` and index is ${keyPressNum} and `  + ' & correct key count is ' + correctKeys);
     }
+     keyPressNum += 1 ;
     
     if (correctKeys === codes.length) {
       alert ('Well done!!');
+    } else {
+      if (keyPressNum === 10 && correctKeys != codes.length) {
+        alert ('NOPE - NOT RIGHT');
+        correctKeys = 0;
+        keyPressNum = 0;
+        
+      } 
     }
+    
   })
   
 }
